f16027592c0fd2882004b9b62fbaa9d3ff63a945
MartinlLee <yanzhou.li@my.jcu.edu.au>
2019-12-13 19:29:01 +0800

    First, add a color dictionary, and then ask the user to enter the color to be searched. If the color to be searched is not in the dictionary, a message will be printed to remind the user that the color to be searched is not in the dictionary.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,9 +1,11 @@
-COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
-                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
-                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7",
+                "antiquewhite1": "#ffefdb", "antiquewhite2": "#eedfcc",
+                "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378",
                 "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
                 "aquamarine4": "#458b74", "azure1": "#f0ffff"}
-colour_name = input("Please enter a colour name: ")
+
+colour_name = input("Please enter a colour name: ").lower()
 while colour_name != "":
     print("The code for {} is {}".format(colour_name,COLOUR_CODES.get(colour_name)))
-    colour_name = input("Please enter a colour name: ")
+    colour_name = input("Please enter a colour name: ").lower()
+
